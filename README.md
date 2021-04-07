bc70a495d238e115b46a24fd7aad7ece39626443
Chris Nesbitt-Smith <chris@cns.me.uk>
2021-04-07 21:36:45 +0100

    updates

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 my own notes on following https://www.udemy.com/course/certified-kubernetes-security-specialist/ and working towards my CKS
 
-- [build.sh](./build.sh) deploy the base 2 node cluster in gke
-- [destroy.sh](./destroy.sh) destroy the k8s nodes
-- [stop.sh](./stop.sh) stop the nodes
-- [stop.sh](./stop.sh) start the nodes
+| script                     | approximate execution time | purpose                               |
+| -------------------------- | -------------------------- | ------------------------------------- |
+| [build.sh](./build.sh)     | 4m12s                      | deploy the base 2 node cluster in GCP |
+| [destroy.sh](./destroy.sh) | 24s                        | destroy the k8s nodes                 |
+| [start.sh](./start.sh)     | 13s                        | stop the nodes                        |
+| [stop.sh](./stop.sh)       | 25s                        | start the nodes                       |
---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+gcloud config set compute/zone europe-west2-c
+
+gcloud compute instances create cks-worker cks-master \
+--machine-type=e2-medium \
+--image=ubuntu-1804-bionic-v20201014 \
+--image-project=ubuntu-os-cloud \
+--preemptible \
+--resource-policies=shutdown \
+--boot-disk-size=50GB
+
+set -e
+
+echo sleeping 60s
+sleep 60
+
+# until gcloud compute ssh cks-master --zone=europe-west2-c --command="echo" >/dev/null 2>&1; do echo -n "."; done
+# until gcloud compute ssh cks-worker --zone=europe-west2-c --command="echo" >/dev/null 2>&1; do echo -n "."; done
+
+# @TODO: a better way to do this would be to run the scripts with the metadata thing in gcloud and then poll for when that finishes before grabbing the join token and sending it to the worker
+
+curl -s https://raw.githubusercontent.com/killer-sh/cks-course-environment/master/cluster-setup/latest/install_worker.sh | gcloud compute ssh cks-worker -- sudo bash
+
+curl -s https://raw.githubusercontent.com/killer-sh/cks-course-environment/master/cluster-setup/latest/install_master.sh | gcloud compute ssh cks-master -- sudo bash
+
+TOKEN=$(gcloud compute ssh cks-master --command="sudo kubeadm token create --print-join-command --ttl 0")
+
+echo $TOKEN | gcloud compute ssh cks-worker -- sudo bash
+
+gcloud compute ssh cks-master --command="sudo kubectl get nodes"
